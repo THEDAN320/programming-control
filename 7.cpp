72968eab1be605ff4227c3fcc16890bc0f7b9342
THEDAN320 <103934625+THEDAN320@users.noreply.github.com>
2022-11-16 12:38:54 +0700

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -2,48 +2,60 @@
 
 using namespace std;
 
-void output(string otvet){
-    cout<<otvet;
+void output(string otvet)
+{
+    cout << otvet;
 }
 
-void solution(int k, int b){
-    string m[100][100];
+void solution(int k, int b)
+{
+    int y = 20 * k + b;
+    string m[y][40];
 
-    for(int y = 0; y < 100;y++){
-        for(int x = 0; x < 100;x++){
-            m[y][x] = " ";
+    for (int y1 = 0; y1 < y; y1++)
+    {
+        for (int x = 0; x < 40; x++)
+        {
+            m[y1][x] = " ";
         }
     }
 
-    for(int x = 0;x < 100;x++){
-        m[80][x] = "-";
+    for (int x = 0; x < 40; x++)
+    {
+        m[20][x] = "-";
     }
 
-    for(int y = 0;y < 100;y++){
-        m[y][20] = "|";
+    for (int y2 = 0; y2 < y; y2++)
+    {
+        m[y2][20] = "|";
     }
 
-    for(int x = 0; x < 20; x++){
-        int y = x*k + b;
-        m[100 - y][x + 10] = "/";
+    for (int x = -10; x < 10; x++)
+    {
+        int yn = x * k + b;
+        m[20 -  yn][x + 20] = "/";
     }
 
-    for(int y = 0; y < 100;y++){
-        for(int x = 0; x < 100;x++){
-            cout<<m[y][x];
+    for (int y3 = 0; y3 < y; y3++)
+    {
+        for (int x = 0; x < 40; x++)
+        {
+            cout << m[y3][x];
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-void input(){
+void input()
+{
     int k, b;
-    cin>>k>>b;
+    cin >> k >> b;
 
     solution(k, b);
 }
 
-int main(){
+int main()
+{
     input();
     return 0;
 }
